d3a7fa7945fd71d8ece6a85cb56097b73ee5808e
Cy Bae <cybae0804@gmail.com>
2019-12-24 16:12:16 -0800

    console logs ALL saved posts

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import qs from 'query-string';
 import { withRouter } from 'react-router-dom';
+import { toJS } from 'mobx';
 import { observer, inject } from 'mobx-react';
 
 @inject('store')
@@ -17,7 +18,7 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.props.store.reddit.savedPosts);
+    console.log(toJS(this.props.store.reddit.savedPosts));
     return (
       <div className="App">
         <Button onClick={this.props.store.reddit.initiateOAuth}>
