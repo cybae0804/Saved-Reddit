406cf38a991d010695cacbdf549befb9daaef52b
Cy Bae <cybae0804@gmail.com>
2019-12-25 14:37:02 -0800

    Hooked up the button again

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,13 @@
-import React, { Component } from 'react';
-import { Button, Container } from 'semantic-ui-react';
-import { toJS } from 'mobx';
-import { observer, inject } from 'mobx-react';
+import React from 'react';
 
 import NavBar from 'components/NavBar';
 import Landing from 'components/Landing';
 
-@inject('store')
-@observer
-class App extends Component {
-  render() {
-    console.log(toJS(this.props.store.reddit.savedPosts));
-    return (
-      <div className="App">
-        <NavBar />
-        <Landing />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <NavBar />
+    <Landing />
+  </div>
+);
 
 export default App;
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,41 +2,47 @@ import React from 'react';
 import {
   Header, Button, Segment, Icon, Image,
 } from 'semantic-ui-react';
+import { toJS } from 'mobx';
+import { observer, inject } from 'mobx-react';
+
 import image from 'assets/Search_SVG.svg';
 
-const Landing = () => (
-  <Segment
-    padded="very"
-    size="massive"
-    color="blue"
-    inverted
-    basic
-    style={{
-      margin: 0,
-      minHeight: 'calc(100vh - 40px)',
-      padding: '10rem',
-    }}
-  >
-    <Image
-      src={image}
-      size="large"
-      centered
-    />
-    <Header
-      size="huge"
-      textAlign="center"
-    >
-      Organize your Saved Posts from Reddit
-      <Header.Subheader style={{ marginTop: '2rem' }}>
-        <Button basic inverted animated="fade">
-          <Button.Content visible>Authenticate</Button.Content>
-          <Button.Content hidden style={{ transform: 'translateY(-50%)', marginTop: 0 }}>
-            <Icon name="reddit" fitted size="large" />
-          </Button.Content>
-        </Button>
-      </Header.Subheader>
-    </Header>
-  </Segment>
-);
+const Landing = (props) => {
+  console.log(toJS(props.store.reddit.savedPosts));
 
-export default Landing;
+  return (
+    <Segment
+      padded="very"
+      size="massive"
+      color="blue"
+      inverted
+      basic
+      style={{
+        margin: 0,
+        minHeight: 'calc(100vh - 40px)',
+        padding: '10rem',
+      }}
+    >
+      <Image
+        src={image}
+        size="large"
+        centered
+      />
+      <Header
+        size="huge"
+        textAlign="center"
+      >
+        Organize your Saved Posts from Reddit
+        <Header.Subheader style={{ marginTop: '2rem' }}>
+          <Button basic inverted animated="fade" onClick={props.store.reddit.initiateOAuth}>
+            <Button.Content visible>Authenticate</Button.Content>
+            <Button.Content hidden style={{ transform: 'translateY(-50%)', marginTop: 0 }}>
+              <Icon name="reddit" fitted size="large" />
+            </Button.Content>
+          </Button>
+        </Header.Subheader>
+      </Header>
+    </Segment>);
+};
+
+export default inject('store')(observer(Landing));
