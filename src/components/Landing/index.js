bdc253ca67196371b02618818425fbf671df8cb9
Cy Bae <cybae0804@gmail.com>
2019-12-27 12:10:43 -0800

    loads saved posts

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,44 +2,39 @@ import React from 'react';
 import {
   Header, Button, Icon, Image,
 } from 'semantic-ui-react';
-import { toJS } from 'mobx';
 import { observer, inject } from 'mobx-react';
 
 import image from 'assets/Search_SVG.svg';
 
-const Landing = (props) => {
-  console.log(toJS(props.store.reddit.savedPosts));
+const Landing = (props) => (
+  <div
+    style={{
+      position: 'absolute',
+      left: '50%',
+      top: '40%',
+      transform: 'translate(-50%, -50%)',
+    }}
+  >
+    <Image
+      src={image}
+      size="large"
+      centered
+    />
 
-  return (
-    <div
-      style={{
-        position: 'absolute',
-        left: '50%',
-        top: '40%',
-        transform: 'translate(-50%, -50%)',
-      }}
+    <Header
+      size="huge"
+      textAlign="center"
     >
-      <Image
-        src={image}
-        size="large"
-        centered
-      />
-
-      <Header
-        size="huge"
-        textAlign="center"
-      >
-        Organize your Saved Posts from Reddit
-        <Header.Subheader style={{ marginTop: '2rem' }}>
-          <Button basic inverted animated="fade" onClick={props.store.reddit.initiateOAuth}>
-            <Button.Content visible>Authenticate</Button.Content>
-            <Button.Content hidden style={{ transform: 'translateY(-50%)', marginTop: 0 }}>
-              <Icon name="reddit" fitted size="large" />
-            </Button.Content>
-          </Button>
-        </Header.Subheader>
-      </Header>
-    </div>);
-};
+      Organize your Saved Posts from Reddit
+      <Header.Subheader style={{ marginTop: '2rem' }}>
+        <Button basic inverted animated="fade" onClick={props.store.reddit.initiateOAuth}>
+          <Button.Content visible>Authenticate</Button.Content>
+          <Button.Content hidden style={{ transform: 'translateY(-50%)', marginTop: 0 }}>
+            <Icon name="reddit" fitted size="large" />
+          </Button.Content>
+        </Button>
+      </Header.Subheader>
+    </Header>
+  </div>);
 
 export default inject('store')(observer(Landing));
--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
+import { toJS } from 'mobx';
+import { observer, inject } from 'mobx-react';
+import {
+ Container, Input, Item, Select, Button, Segment 
+} from 'semantic-ui-react';
 
-const Posts = () => <div />;
+const Posts = (props) => {
+  console.log(toJS(props.store.reddit.savedPosts));
 
-export default Posts;
+  return <Container>
+    <Input
+      fluid
+      type='text'
+      placeholder='Search...'
+      icon='search'
+      style={{ marginBottom: '2rem' }}
+    />
 
+    <Item.Group relaxed>
+      {
+        props.store.reddit.savedPosts.map((post) => (
+        <Item key={post.id}>
+          <Item.Image size="tiny" src={post?.preview?.images?.[0]?.source?.url} />
+
+          <Item.Content>
+            <Item.Header>{post?.title ?? post?.link_title}</Item.Header>
+            <Item.Description>{post?.body ?? ''}</Item.Description>
+          </Item.Content>
+        </Item>))
+      }
+    </Item.Group>
+  </Container>;
+};
+
+export default inject('store')(observer(Posts));
